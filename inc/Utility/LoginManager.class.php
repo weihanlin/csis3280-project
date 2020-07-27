062e5a7530df4a4f38f161e93a3344a40e91111f
Sylvi <38122325+rioot123@users.noreply.github.com>
2020-07-26 18:17:25 -0700

    Add files via upload

--- inc/Utility/LoginManager.class.php
+++ inc/Utility/LoginManager.class.php
@@ -8,22 +8,28 @@ class LoginManager  {
             session_start();
         }
         if(isset($_SESSION["email"])){
-
             return true;
         }
 
         else{
-       
             session_destroy();
-
-
             header("Location: parkingLogin.php");
-
             return false;
-
         }
     }
-        
+    static function verifyAdmin(){
+        if(session_id() == "" && !isset($_SESSION)){
+            session_start();
+        }
+        if($_SESSION["isAdmin"] == true){
+            return true;
+        }
+        else{
+            session_destroy();
+            header("Location: parkingLogin.php");
+            return false;
+        }
+    }
     
 }
 
