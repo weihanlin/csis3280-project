f41c84f485cedc8cff4bf6b83f164def430fdbce
Sylvi <38122325+rioot123@users.noreply.github.com>
2020-07-27 16:48:17 -0700

    Update Page.class.php

--- inc/Entity/Page.class.php
+++ inc/Entity/Page.class.php
@@ -395,7 +395,7 @@ class Page {
         </tr>
         <tr>
             <td align="right"><label for="phone">Phone Number (xxx-xxx-xxxx)</label></td>
-            <td align="left"><input type="tel" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"></td>
+            <td align="left"><input type="tel" name="phonenumber" pattern="[0-9]{3}[0-9]{3}[0-9]{4}"></td>
         </tr>
         <tr>
         </tr>
