caf77d22316e3419bad400405adca78bc33f8dff
Sylvi <38122325+rioot123@users.noreply.github.com>
2020-07-26 17:59:58 -0700

    Login Page

---
+++ parkingLogin.php
@@ -0,0 +1,47 @@
+<?php
+
+require_once("inc/config.inc.php");
+require_once("inc/Entity/Page.class.php");
+require_once("inc/Entity/User.class.php");
+
+require_once("inc/Utility/PDOService.class.php");
+require_once("inc/Utility/LoginManager.class.php");
+require_once("inc/Utility/UserDAO.class.php");
+
+
+if(!empty($_POST['email'])){
+
+    UserDAO::initialize();
+    $user = UserDAO::getUser($_POST['email']);
+    if($user instanceof User){
+
+        if($user->verifyPassword($_POST['password'])){
+
+            session_start();
+
+            $_SESSION['email'] = $user->getEmail();
+
+            header("Location: UserProfile.php");
+        }
+        else{
+            Page::header();
+            echo("Invalid Email/Password");
+            Page::displayLogin();
+            Page::footer();
+        }
+    }
+    else{
+        Page::header();
+        echo("Invalid User, Please Register an Account");
+        Page::displayLogin();
+        Page::footer();
+    }
+    }  
+
+else{
+Page::header();
+Page::displayLogin();
+Page::footer();
+}
+
+?>
