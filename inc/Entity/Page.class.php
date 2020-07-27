69c33fceb22e7fa3562ebff1bfc6f65695254258
Sylvi <38122325+rioot123@users.noreply.github.com>
2020-07-27 16:49:47 -0700

    Update Page.class.php

--- inc/Entity/Page.class.php
+++ inc/Entity/Page.class.php
@@ -394,7 +394,7 @@ class Page {
             <td align="left"><input type="text" name="fullname" required></td>
         </tr>
         <tr>
-            <td align="right"><label for="phone">Phone Number (xxx-xxx-xxxx)</label></td>
+            <td align="right"><label for="phone">Phone Number (no hyphens)</label></td>
             <td align="left"><input type="tel" name="phonenumber" pattern="[0-9]{3}[0-9]{3}[0-9]{4}"></td>
         </tr>
         <tr>
